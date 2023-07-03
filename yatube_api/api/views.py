8335f87d5cda8f6c8d1ba9fc531ad17d991119b7
Николай Екатеринин <eh37@ya.ru>
2023-07-03 23:48:08 +0300

    add router, PostViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from posts.models import Post, Group, Comment
+from posts.serializers import PostSerializer, CommentSerializer, GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,11 +2,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
+
+from rest_framework import routers
+from api.views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register('api/v1/posts/', PostViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api/', include('api.urls'))
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('api/v1/posts/', include(router.urls)),
+    # path('api/v1/groups/', name='groups'),
+    # path('api/v1/groups/{group_id}/', name='groups_detail'),
+    # path('api/v1/posts/{post_id}/comments/', name='post_comments'),
+    # path('api/v1/posts/{post_id}/comments/{comment_id}/',
+    #     name='comments_edit'),
 ]
 
 
